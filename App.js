af95c34e2e8bdff343fa93e332e02c1dd353ed3a
jox404 <email@example.com>
2022-06-27 11:32:48 -0300

    teste

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from './src/routes';
+/* Fonts */
+import { useFonts } from 'expo-font';
+/* Icons */
+import {Ionicons} from '@expo/vector-icons/'
+
+
 export default function App() {
+
+  let [loader] = useFonts({
+    ontserratItalic: require('./src/assets/fonts/Montserrat-Italic-VariableFont_wght.ttf'),
+    Montserrat: require('./src/assets/fonts/static/Montserrat-Light.ttf'),
+    MontserratBold: require('./src/assets/fonts/static/Montserrat-Bold.ttf'),
+    MontserratSemiBold: require('./src/assets/fonts/static/Montserrat-SemiBold.ttf'),
+    MontserratLight: require('./src/assets/fonts/static/Montserrat-ExtraLight.ttf'),
+  })
+
+  if(!loader){
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <>
+  <StatusBar style='light' backgroundColor='#000' translucent={false}/>
+      <Routes/>
+  </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
